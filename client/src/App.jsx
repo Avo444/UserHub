9d746f4f403cde84e75fe3f934f4770267f6b1ba
Avo444 <avetiqm8@gmail.com>
2026-04-27 13:06:07 +0400

    add Profile view

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,11 @@ import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
-import { AddUserPage, Home, NotFound } from "./pages";
-import { Navbar } from "./components/features";
-
-import { getUsersData } from "./store/slices";
 import { ROUTES } from "./constants";
+import { Navbar } from "./components/features";
+import { getUsersData } from "./store/slices";
+import { AddUserPage, Home, NotFound, View } from "./pages";
+
 
 import "./App.scss";
 
@@ -24,6 +24,7 @@ const App = () => {
             <Routes>
                 <Route path={ROUTES.HOME} element={<Home />} />
                 <Route path={ROUTES.ADD_USER} element={<AddUserPage />} />
+                <Route path={`${ROUTES.VIEW}${ROUTES.EACH_VIEW}`} element={<View />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
 
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,6 @@
 import Home from "./Home";
+import View from "./View";
 import NotFound from "./NotFound";
 import AddUserPage from "./AddUser";
 
-export { Home, NotFound, AddUserPage };
+export { View, Home, NotFound, AddUserPage };
